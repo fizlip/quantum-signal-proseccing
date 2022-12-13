704c5ee525765603b947efdac363b737fb137104
fizlip <fillatino@gmail.com>
2022-12-13 12:43:23 +0100

    update README

--- README.md
+++ README.md
@@ -2,14 +2,29 @@
 Quantum signal processing is built on interleaving 2 types of single-qubit rotations. A signal rotation operator and a signal processing operator. This repository includes classes that a demonstrate how these are combined into QSP control sequence which allow us to do signal processing on qubits.
 
 # Usage
+
 The script has two functions, `qsp_seq` and `qsp`. `qsp_sequence` evaluates the
 QSP sequence as defined in the paper by Martyn et.al. and demonstrates how 
 transitions probabilities can be modified using the signal rotation operator,
 W and the signal processing operator S.
 
-To see how the transition probabilities are affected by the rotation operator
-provide a angle sequence \phi:
+## QSP Sequence
+To evaluate the transition probabilities using the rotation operator and signal
+operator you need to provide an phase angle sequence \phi:
 
 ```
-$ python main.py -qsp_seq (math.pi/2, 0.5*math.acos(-0.25), math.acos(-0.25), 0, -2*math.acos(-0.25), 0.5*math.acos(-0.25)) 
+$ main.py -qsp_seq 1,1,1,1
 ```
+Here 1,1,1,1 is a simple angle sequence used as an example. This command 
+plots the transition probabilities and compares it to the trivial case where
+phi = (0,0) (i.e. no signal processing)
+
+
+The BB1 filter has also been implemented and can be compared with the trivial
+case:
+```
+$ main.py -qsp_seq bb1
+```
+
+## QSP
+
--- main.py
+++ main.py
@@ -8,27 +8,56 @@ from response import PlotQSPResponse
 from poly import StringPolynomial
 
 def main():
+    '''
+    Main function. Commands with corresponding arguments. 
+    '''
+
+    help_text = """
+    Quantum Signal Processing for seminar Advanced Topics of Quantum Computing [IN2107]
+
+    usage: main.py cmd args
+    Commands:
+        qsp_seq - compute the QSP sequence for a set of phase angles and plot the result
+        qsp - compute the phase angles for given polynomial and plot the result
+    Examples:
+        main.py -qsp_seq 1,1,1,1
+        main.py -qsp_seq bb1
+        main.py -qsp 'x**2'
+        main.py -qsp 'np.cos(x**2)'
+    """
+
     args = sys.argv[1:]
     cmd = args[0][1:]
     arg = args[1:]
 
+    if (cmd == "help"):
+        print(help_text)
+
     if(cmd == "qsp_seq"):
+
         if(arg[0] == "bb1"):
             eta = 0.5*math.acos(-0.25)
             phis = [math.pi/2, -eta, 2*eta, 0, -2*eta, eta]
         else:
             phis = [int(i) for i in arg[0].split(",")]
+
         print(f"[main.py] Evaluating QSP sequence for: {phis}")
         data = Evaluate(phis)
         PlotResponse(data)
+
     if(cmd == "qsp"):
+
         print(f"[main.py] Calculating phis for: {arg[0]}") 
+        # Change degree of polynomial by passing an extra argument. 
         if(len(arg) == 2):
             poly = StringPolynomial(arg[0], arg[1])
         else:
             poly = StringPolynomial(arg[0], 10)
          
+        # Here we use TensorFlor 'tf' to approximate phases
         ang_seq = QuantumSignalProcessingPhases(poly, method="tf")
+
+        # This function plots the approximated polynomial and compares it to target
         PlotQSPResponse(ang_seq, target=poly, signal_operator="Wx")
 
 if __name__ == '__main__':
